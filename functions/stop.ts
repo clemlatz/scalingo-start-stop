06f52643f185c17ca98568b407f6e44345b4ec03
Clément Latzarus <c@ltzr.io>
2025-01-06 23:03:23 +0100

    feat: add stop function

---
+++ functions/stop.ts
@@ -0,0 +1,31 @@
+import {schedule} from '@netlify/functions'
+import {clientFromToken} from 'scalingo';
+
+export const handler = schedule('@hourly', async () => {
+  const {SCALINGO_API_TOKEN, SCALINGO_APP_ID} = process.env;
+
+  const client = await clientFromToken(SCALINGO_API_TOKEN, {});
+  const {status} = await client.Apps.find(SCALINGO_APP_ID);
+
+  console.log(`Application status: ${status}`);
+
+  if (status === 'stopped' || status === 'scaling') {
+    console.log(`Doing nothing`);
+    return {
+      statusCode: 204,
+    };
+  }
+
+  console.log('Scaling application to 0…');
+  await client.Apps._client.apiClient().post(`/apps/${SCALINGO_APP_ID}/scale`, {
+    containers: [{
+      name: 'web',
+      amount: 0,
+    }]
+  });
+  console.log('Application was scaled to 0');
+
+  return {
+    statusCode: 201,
+  }
+})
